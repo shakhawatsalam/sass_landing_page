df3edf7ec47be53960fe32064495b80f858b16ef
unknown <shakhawatsalam726@gmail.com>
2024-11-05 08:31:22 +0530

    [Mobile Navigation and header section is done]

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,17 @@ import { HTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
 export type ButtonProps = {
-  variant: "primary" | "secondary" | "tertiary";
+  variant?: "primary" | "secondary" | "tertiary";
+  block?: boolean;
 } & HTMLAttributes<HTMLButtonElement>;
 
 const classes = cva(
   "text-xs tracking-widest uppercase font-bold h-10 px-6 rounded-lg",
   {
     variants: {
+      block: {
+        true: "w-full",
+      },
       variant: {
         primary:
           "border-2 border-transparent [background:linear-gradient(var(--color-gray-950),var(--color-gray-950))_padding-box,conic-gradient(from_45deg,var(--color-violet-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400))_border-box]",
@@ -19,6 +23,7 @@ const classes = cva(
 
     defaultVariants: {
       variant: "primary",
+      block: false,
     },
   }
 );
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,9 @@
 "use client";
 import logoImage from "@/assets/images/sphereal-logo.svg?url";
 import { Button, ButtonProps } from "@/components/Button";
+import { Orbit } from "@/components/Orbit";
+import { a } from "framer-motion/client";
+
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 export const navItems = [
@@ -40,7 +43,7 @@ export const Header = () => {
 
   return (
     <>
-      <header className="border-b border-gray-200/20">
+      <header className="border-b border-gray-200/20 relative z-40">
         <div className="container">
           <div className="h-18 lg:h-20 flex items-center justify-between">
             <div className="flex gap-4 items-center">
@@ -78,7 +81,7 @@ export const Header = () => {
                 className="size-10 rounded-lg border-2 border-transparent [background:linear-gradient(var(--color-gray-950),var(--color-gray-950))_content-box,conic-gradient(from_45deg,var(--color-violet-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400))_border-box]  relative"
                 onClick={() => setIsMobileNavOpen((curr) => !curr)}
               >
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="absolute-center">
                   <div
                     className={twMerge(
                       "w-4 h-0.5 bg-gray-100 -translate-y-1 transition duration-300",
@@ -87,7 +90,7 @@ export const Header = () => {
                     )}
                   ></div>
                 </div>
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="absolute-center">
                   <div
                     className={twMerge(
                       "w-4 h-0.5 bg-gray-100 translate-y-1 transition duration-300",
@@ -101,6 +104,50 @@ export const Header = () => {
           </div>
         </div>
       </header>
+
+      {isMobileNavOpen && (
+        <div className="fixed top-18 left-0 bottom-0 right-0 bg-gray-950 z-30 overflow-hidden">
+          {/* orbit-1  */}
+          <div className="absolute-center isolate -z-10">
+            <Orbit />
+          </div>
+          {/* orbit-2  */}
+          <div className="absolute-center isolate -z-10">
+            <Orbit className="size-[350px]" />
+          </div>
+          {/* orbit-3  */}
+          <div className="absolute-center isolate -z-10">
+            <Orbit className="size-[500px]" />
+          </div>
+          {/* orbit-4  */}
+          <div className="absolute-center isolate -z-10">
+            <Orbit className="size-[650px]" />
+          </div>
+          <div className="absolute-center isolate -z-10">
+            <Orbit className="size-[800px]" />
+          </div>
+          <div className="container h-full">
+            <nav className="flex flex-col items-center gap-4 py-8 h-full justify-center">
+              {navItems.map(({ name, href }) => (
+                <a
+                  href={href}
+                  key={href}
+                  className="text-gray-400 uppercase tracking-widest font-bold text-xs h-10"
+                >
+                  {name}
+                </a>
+              ))}
+              {loginItems.map(({ buttonVariant, name, href }) => (
+                <a href={href} key={href} className="w-full max-w-xs">
+                  <Button block variant={buttonVariant}>
+                    {name}
+                  </Button>
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      )}
     </>
   );
 };
